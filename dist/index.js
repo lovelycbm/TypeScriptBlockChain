01590ece1f741d30217df7f665b6f21b66637b56
lovelycbm <lovelycbm@naver.com>
2020-09-21 23:14:05 +0900

    last_commit

--- dist/index.js
+++ dist/index.js
@@ -11,6 +11,11 @@ class Block {
     }
 }
 Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+Block.validateStructure = (aBlock) => typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
 const genesisBlock = new Block(0, "64981516", "", "hello", 123456);
 // 블록체인 클래스 한개 생성해서 블록 어레이에 넣은겨?
 let blockchain = [genesisBlock];
@@ -23,7 +28,35 @@ const createNewBlock = (data) => {
     const newTimeStamp = getNewTimeStamp();
     const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimeStamp, data);
     const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimeStamp);
+    addBlock(newBlock);
     return newBlock;
 };
-console.log(createNewBlock("Hello"), createNewBlock("Bye"));
+const getHashforBlock = (aBlock) => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+const isBlockValid = (candidateBlock, previousBlock) => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    }
+    else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    }
+    else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
+    else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+const addBlock = (candidateBlock) => {
+    if (isBlockValid(candidateBlock, getLatestBlock())) {
+        blockchain.push(candidateBlock);
+    }
+};
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+console.log(blockchain);
+console.log(getBlockChain());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,13 @@ class Block{
         string=>
         CryptoJS.SHA256(index+previousHash+timestamp+data).toString();
 
+    static validateStructure = (aBlock: Block) : boolean => 
+    typeof aBlock.index === "number" && 
+    typeof aBlock.hash === "string" && 
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+
     constructor(
         index:number,
         hash:string,
@@ -60,10 +67,39 @@ const createNewBlock = (data:string) : Block => {
         newHash,
         previousBlock.hash,
         data,newTimeStamp)
-
+    addBlock(newBlock)
     return newBlock;
 };
 
-console.log(createNewBlock("Hello"),createNewBlock("Bye"));
 
+
+const getHashforBlock = (aBlock:Block) : string => Block.calculateBlockHash(aBlock.index,aBlock.previousHash,aBlock.timestamp,aBlock.data);
+
+const isBlockValid = (candidateBlock : Block, previousBlock: Block) : boolean =>{
+    if(!Block.validateStructure(candidateBlock)) {
+        return false;
+    } else if (previousBlock.index +1 !== candidateBlock.index){
+        return false; 
+    } else if (previousBlock.hash !== candidateBlock.previousHash){
+        return false;
+    } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash){
+        return false;
+    } else{
+        return true;
+    }
+}
+
+const addBlock = (candidateBlock : Block) : void => {
+    if(isBlockValid(candidateBlock, getLatestBlock())){
+        blockchain.push(candidateBlock);
+    }
+}
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
+
+console.log(getBlockChain());
 export {};
